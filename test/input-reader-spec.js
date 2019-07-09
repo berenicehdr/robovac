d55e9262d693cf2045bc6d77840d34f3a9c7ac15
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-07-09 14:37:49 +0100

    remove unnecessary usage of JSON#Stringify by changing assertion matcher

--- test/input-reader-spec.js
+++ test/input-reader-spec.js
@@ -6,9 +6,9 @@ var InputReader = require('../src/input-reader')
 describe('InputReader', function () {
   it('parses the room dimesions from the supplied input file', function () {
     let input = new InputReader()
-    let roomDimensions = JSON.stringify(input.roomDimensions)
-    let expectedRoomDimensions = JSON.stringify({ x: 5, y: 5 })
-    assert.strictEqual(roomDimensions, expectedRoomDimensions)
+    let roomDimensions =input.roomDimensions
+    let expectedRoomDimensions = { x: 5, y: 5 }
+    assert.deepStrictEqual(roomDimensions, expectedRoomDimensions)
   })
 
   it('parses the vacuum start position from the supplied input file', function () {
